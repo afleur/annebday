dfe88ed3084b0daa800521977bc6bb61af24b983
Alois Fleuricourt <afleuricourt@gmail.com>
2019-09-23 22:15:17 +0200

    text added & JS bug fixed

--- app/javascript/plugins/swag.js
+++ app/javascript/plugins/swag.js
@@ -2,7 +2,7 @@
 
     const swag_ans = () => {
       const third_block = document.querySelector('#third_block');
-      const swag_answer = document.querySelector('.swag_answer');
+      const swag_answer = document.querySelector('#swag_answer');
       const fourth_block = document.querySelector('#fourth_block');
       const fifth_block = document.querySelector('#fifth_block');
       const girl_answer = document.querySelector('.girl_answer');
@@ -10,8 +10,7 @@
       third_block.classList.add("display_none");
       girl_answer.classList.add("display_none");
       swag_answer.classList.remove("display_none");
-      fourth_block.style.display = "";
-      fifth_block.classList.remove("display_none");
+      fourth_block.classList.remove("display_none");
       };
 
     const swag_btn = document.querySelector('#swag_btn');
@@ -20,8 +19,13 @@
 
     const notswag_ans = () => {
       const third_block = document.querySelector('#third_block');
-      const notswag_answer = document.querySelector('.notswag_answer');
+      const notswag_answer = document.querySelector('#notswag_answer');
       const fourth_block = document.querySelector('#fourth_block');
+      const girl_answer = document.querySelector('.girl_answer');
+      const boy_answer = document.querySelector('.boy_answer');
+
+      girl_answer.classList.add("display_none");
+      boy_answer.classList.add("display_none");
       third_block.classList.add("display_none");
       notswag_answer.classList.remove("display_none");
       fourth_block.classList.remove("display_none");
